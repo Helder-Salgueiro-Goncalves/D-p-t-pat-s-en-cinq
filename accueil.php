c3cc5a2a530959512ad4f5d63c7b1d2466548d40
Helder Salgueiro-Gonçalves <salgueiro.goncalves.60@gmail.com>
2024-06-05 17:01:32 +0200

    Finalisation page Accueil + début page A propos

--- accueil.php
+++ accueil.php
@@ -23,26 +23,35 @@ $titrePrestationAccueil4->execute();
 $PrestationAccueilTitre4 = $titrePrestationAccueil4->fetch();
 $PrestationAccueilTitre4 = $PrestationAccueilTitre4['prestation_libelle'];
 
+
+
+
 // Récupération de la description1 de la partie 'PRESTATION' de l'accueil
-$descriptionPrestationAccueil1 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_contenu ASC LIMIT 1');
+$descriptionPrestationAccueil1 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_libelle ASC LIMIT 1');
 $descriptionPrestationAccueil1->execute();
 $PrestationAccueil1 = $descriptionPrestationAccueil1->fetch();
 $PrestationAccueil1 = $PrestationAccueil1['prestation_contenu'];
+
 // Récupération de la description2 de la partie 'PRESTATION' de l'accueil
-$descriptionPrestationAccueil2 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_contenu ASC LIMIT 1, 2');
+$descriptionPrestationAccueil2 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_libelle ASC LIMIT 1, 2');
 $descriptionPrestationAccueil2->execute();
 $PrestationAccueil2 = $descriptionPrestationAccueil2->fetch();
 $PrestationAccueil2 = $PrestationAccueil2['prestation_contenu'];
+
 // Récupération de la description3 de la partie 'PRESTATION' de l'accueil
-$descriptionPrestationAccueil3 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_contenu ASC LIMIT 2, 3');
+$descriptionPrestationAccueil3 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_libelle ASC LIMIT 2, 3');
 $descriptionPrestationAccueil3->execute();
 $PrestationAccueil3 = $descriptionPrestationAccueil3->fetch();
 $PrestationAccueil3 = $PrestationAccueil3['prestation_contenu'];
+
 // Récupération de la description4 de la partie 'PRESTATION' de l'accueil
-$descriptionPrestationAccueil4 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_contenu ASC LIMIT 3, 4');
+$descriptionPrestationAccueil4 = $connexion->prepare('SELECT prestation_contenu FROM prestation ORDER BY prestation_libelle ASC LIMIT 3, 4');
 $descriptionPrestationAccueil4->execute();
 $PrestationAccueil4 = $descriptionPrestationAccueil4->fetch();
 $PrestationAccueil4 = $PrestationAccueil4['prestation_contenu'];
+
+
+
 
 // Récupération de la description de la partie 'METHODE' de l'accueil
 $descriptionMethodeAccueil = $connexion->prepare('SELECT meth_descriptif FROM methode');
@@ -51,46 +60,52 @@ $MethodeAccueil = $descriptionMethodeAccueil->fetch();
 $MethodeAccueil = $MethodeAccueil['meth_descriptif'];
 
 // Récupération du nom1 de la partie 'PARTENAIRE' de l'accueil
-$nomPartenaireAccueil1 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_libelle ASC LIMIT 1,2');
+$nomPartenaireAccueil1 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_id DESC LIMIT 1');
 $nomPartenaireAccueil1->execute();
 $PartenaireAccueil1 = $nomPartenaireAccueil1->fetch();
 $PartenaireAccueil1 = $PartenaireAccueil1['part_libelle'];
 // Récupération du nom2 de la partie 'PARTENAIRE' de l'accueil
-$nomPartenaireAccueil2 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_libelle DESC LIMIT 1');
+$nomPartenaireAccueil2 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_id DESC LIMIT 1, 2');
 $nomPartenaireAccueil2->execute();
 $PartenaireAccueil2 = $nomPartenaireAccueil2->fetch();
 $PartenaireAccueil2 = $PartenaireAccueil2['part_libelle'];
 // Récupération du nom3 de la partie 'PARTENAIRE' de l'accueil
-$nomPartenaireAccueil3 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_libelle ASC LIMIT 3, 4');
+$nomPartenaireAccueil3 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_id DESC LIMIT 3, 4');
 $nomPartenaireAccueil3->execute();
 $PartenaireAccueil3 = $nomPartenaireAccueil3->fetch();
 $PartenaireAccueil3 = $PartenaireAccueil3['part_libelle'];
 // Récupération du nom4 de la partie 'PARTENAIRE' de l'accueil
-$nomPartenaireAccueil4 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_libelle ASC LIMIT 4, 5');
+$nomPartenaireAccueil4 = $connexion->prepare('SELECT part_libelle FROM partenaire ORDER BY part_id DESC LIMIT 4, 5');
 $nomPartenaireAccueil4->execute();
 $PartenaireAccueil4 = $nomPartenaireAccueil4->fetch();
 $PartenaireAccueil4 = $PartenaireAccueil4['part_libelle'];
 
+
+
+
 // Récupération de la description1 de la partie 'PARTENAIRE' de l'accueil
-$descriptionPartenaireAccueil1 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_descriptif ASC LIMIT 1');
+$descriptionPartenaireAccueil1 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_id DESC LIMIT 1');
 $descriptionPartenaireAccueil1->execute();
 $PartenaireAccueilDescription1 = $descriptionPartenaireAccueil1->fetch();
 $PartenaireAccueilDescription1 = $PartenaireAccueilDescription1['part_descriptif'];
 // Récupération de la description2 de la partie 'PARTENAIRE' de l'accueil
-$descriptionPartenaireAccueil2 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_descriptif ASC LIMIT 1, 2');
+$descriptionPartenaireAccueil2 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_id DESC LIMIT 1, 2');
 $descriptionPartenaireAccueil2->execute();
 $PartenaireAccueilDescription2 = $descriptionPartenaireAccueil2->fetch();
 $PartenaireAccueilDescription2 = $PartenaireAccueilDescription2['part_descriptif'];
 // Récupération de la description3 de la partie 'PARTENAIRE' de l'accueil
-$descriptionPartenaireAccueil3 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_descriptif ASC LIMIT 2, 3');
+$descriptionPartenaireAccueil3 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_id DESC LIMIT 2, 3');
 $descriptionPartenaireAccueil3->execute();
 $PartenaireAccueilDescription3 = $descriptionPartenaireAccueil3->fetch();
 $PartenaireAccueilDescription3 = $PartenaireAccueilDescription3['part_descriptif'];
 // Récupération de la description4 de la partie 'PARTENAIRE' de l'accueil
-$descriptionPartenaireAccueil4 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_descriptif ASC LIMIT 3, 4');
+$descriptionPartenaireAccueil4 = $connexion->prepare('SELECT part_descriptif FROM partenaire ORDER BY part_id DESC LIMIT 3, 4');
 $descriptionPartenaireAccueil4->execute();
 $PartenaireAccueilDescription4 = $descriptionPartenaireAccueil4->fetch();
 $PartenaireAccueilDescription4 = $PartenaireAccueilDescription4['part_descriptif'];
+
+
+
 
 // Récupération du prenom de la partie 'COMMENTAIRE' de l'accueil
 $prenomCommentaireAccueil = $connexion->prepare('SELECT auteur_prenom FROM auteur ORDER BY auteur_prenom ASC LIMIT 1');
@@ -140,7 +155,7 @@ $commentaireAccueildescription = $commentaireAccueildescription['livre_contenu']
                 <img src="./assets/difelice.jpg">
                 <h2>QUI SUIS-JE ?</h2>
                 <p>De ma naissance jusqu'à mon âge actuel, j'ai vécu avec toute sorte d'animaux. J'ai toujours aimé leur compagnie en particulier celles des chiens. J'avais une grande complicité avec nos chiens mais aussi ceux de mon entourage. Passionnée depuis toujours, le travail que j'ai choisi associ plusieurs points très importants pour moi. Avoir un travail passionnant, évolutif et pouvoir aider les gens et les chiens. Pour pouvoir aider au mieux, j'ai souhaité améliorer mes connaissances en effectuant une formation de 8 mois pour obtenir le Brevet Professionnel d'Educateur Canin le seul diplôme reconnu d'état d'actuellement.</p>
-                <a href="./details/details.php">en savoir plus</a>
+                <a class="button-36" href="./details/details.php">en savoir plus</a>
             </aside>
 
             <div class="home-prestation-mere">
@@ -164,7 +179,7 @@ $commentaireAccueildescription = $commentaireAccueildescription['livre_contenu']
                     </div>
                 </div>
                 <div class="home-prestation-btn">
-                    <a href="./prestation/prestation.php">voir plus</a>
+                    <a class="button-36" href="./prestation/prestation.php">voir plus</a>
                 </div>
             </div>
         </div>
@@ -172,11 +187,14 @@ $commentaireAccueildescription = $commentaireAccueildescription['livre_contenu']
 
     <section>
         <div id="slider">
+
+            <div id="precedent" onclick="ChangeSlide(-1)">◄</div>
+
             <img src="assets/dog.jpg" alt="./assets/dog2.jpg" id="slide">
-            <div id="precedent" onclick="ChangeSlide(-1)">
-                </div>
-                    <div id="suivant" onclick="ChangeSlide(1)">></div>
-            </div>
+            
+            <div id="suivant" onclick="ChangeSlide(1)">►</div>
+            
+        </div>
     </section>
     <section>
         <div class="methode">
@@ -209,7 +227,7 @@ $commentaireAccueildescription = $commentaireAccueildescription['livre_contenu']
                     <p><?= $PartenaireAccueilDescription4 ?></p>
                 </div>
             </div>
-            <a href="./partenaire/partenaire.php" class="methode-btn">Voir plus de partenaires</a>
+            <a class="button-36 bouton-partenaire-commentaire" href="./partenaire/partenaire.php">Voir plus de partenaires</a>
         </div>
 
     </section>
@@ -226,7 +244,7 @@ $commentaireAccueildescription = $commentaireAccueildescription['livre_contenu']
                         <i class="fa-solid fa-star"></i>
                     </div>
                 </div>
-                <a href="./livre_d'or/livre.php" class="methode-btn">Voir plus de commentaires</a>
+                <a href="./livre_d'or/livre.php" class="button-36 bouton-partenaire-commentaire">Voir plus de commentaires</a>
 
             </div>
 

8021eff05bee6658023c5d5115f2035d781f0b9c
Helder Salgueiro-Gonçalves <salgueiro.goncalves.60@gmail.com>
2024-06-14 10:40:47 +0200

    Modif pour backoffice

--- backoffice/connexion.php
+++ backoffice/connexion.php
@@ -1,50 +1,8 @@
 <?php
-
 session_start();
-
+require '../includes/connexion_bdd/connexion_bdd.php';
 ?>
 
-<?php
- require '../includes/connexion_bdd/connexion_bdd.php';
-
-	// Etape 1 : Initialisation du tableau d'erreur
-	$erreurs = [];
-
-	// Etape 2 : vérification que les données ont été saisies.
-
-	if (isset($_POST['username']) && isset($_POST['password'])) {
-
-		// Etape 4 : Récupération d'un utilisateur dans la base avec le même nom d'utilisateur (on vérifie qu'il existe)
-
-		$username = $_POST['username'];
-
-		$sth = $connexion->prepare('SELECT * FROM utilisateur WHERE utilisateur_username = :utilisateur_username');
-		$sth->bindParam(':utilisateur_username', $username);
-		$sth->execute();
-
-		$resultat = $sth->fetch();
-
-		if ($resultat === false) {
-			echo 'Identifiants incorrectes.';
-		} else {
-			$password = $_POST['password'];
-
-			// Etape 5 : vérification du mot de passe
-			$verification = password_verify($password, $resultat['utilisateur_mdp']);
-
-			if (!$verification) {
-				echo 'Identifiants incorrectes.';
-			} else {
-				echo 'Vous êtes maintenant connecté.';
-				header('refresh:3; url=backoffice.php');
-			}
-		}
-
-	} else {
-		echo 'Veuillez remplir tous les champs.';
-	}
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -55,15 +13,60 @@ session_start();
 	<title>Connexion</title>
 </head>
 <body>
+	
+	<section class="regroupement_connexion_backlog">
+		
+		<div class="image-gauche">
+			<img src="../assets/logo.jpg" width="200px" height="250px">
+		</div>
+		<div class="regroupement_formulaire">
+			<form class="champs_connexion"  action="#" method="POST">
+				<label for="username">Nom d'utilisateur</label>
+				<input type="text" name="username" id="username">
 
-	<form action="#" method="POST">
-		<label for="username">Nom d'utilisateur</label>
-		<input type="text" name="username" id="username">
+				<label for="password" id="password">Mot de passe</label>
+				<input  id="password" type="password" name="password">
 
-		<label for="password" id="password">Mot de passe</label>
-		<input  id="password" type="password" name="password">
+				<button class="btn btn-primary me-md-2">Connexion</button>
+				<?php
+				// Etape 1 : Initialisation du tableau d'erreur
+				$erreurs = [];
 
-		<button class="btn btn-primary me-md-2">Connexion</button>
-	</form>
+				// Etape 2 : vérification que les données ont été saisies.
+				if (isset($_POST['username']) && isset($_POST['password'])) {
+
+					// Etape 4 : Récupération d'un utilisateur dans la base avec le même nom d'utilisateur (on vérifie qu'il existe)
+					$_SESSION['username'] = $_POST['username'];
+					$username = $_POST['username'];
+
+					$sth = $connexion->prepare('SELECT * FROM utilisateur WHERE utilisateur_username = :utilisateur_username');
+					$sth->bindParam(':utilisateur_username', $username);
+					$sth->execute();
+
+					$resultat = $sth->fetch();
+
+					if ($resultat === false) {
+						echo '<p style="color : red">Identifiants incorrectes.</p>';
+					} else {
+						$password = $_POST['password'];
+
+						// Etape 5 : vérification du mot de passe
+						$verification = password_verify($password, $resultat['utilisateur_mdp']);
+
+						if (!$verification) {
+							echo '<p style="color : red">Identifiants incorrectes.</p>';
+						} else {
+							echo '<p style="color: green;">Vous êtes maintenant connecté.</p>';
+							header('refresh:1.5; url=backoffice.php');
+						}
+					}
+				} else {
+					echo '<p style="color : red">Veuillez remplir tous les champs.</p>';
+				}
+				?>
+			</form>
+			<a href="../index.php" style ="font-size:smaller;">Revenir en arrière</a>
+		</div>
+	</section>
 </body>
 </html>
